a57cff4e84bb79fd5d788ad06b37acf83abcae13
Lakshan1 <lak4837@gmail.com>
2026-07-11 20:48:26 +0530

    Add JSTL dependencies and enhance login functionality with error handling

--- src/main/java/com/Dental/LoginServlet.java
+++ src/main/java/com/Dental/LoginServlet.java
@@ -10,8 +10,23 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
+
+    // Handle GET requests to display the login page
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("login.jsp").forward(request, response);
+    }
+
+    // Handle POST requests to process login form submission
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws  IOException, ServletException {
+
+        // Retrieve form parameters from the request
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        String remember = request.getParameter("remember");
+
+        request.setAttribute("error", email + " does not exists in the system.");
         request.getRequestDispatcher("login.jsp").forward(request, response);
     }
 }
